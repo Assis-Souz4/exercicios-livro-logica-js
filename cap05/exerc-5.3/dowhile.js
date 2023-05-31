6394868685c8e3ab698972f3e7a534d39a36758e
Assis Souz4 <assissouza-dev@hotmail.com>
2023-05-30 23:22:06 -0300

    exerc 5.3

---
+++ cap05/exerc-5.3/dowhile.js
@@ -0,0 +1,13 @@
+let inNumero;
+
+do {
+  inNumero = Number(prompt("Entre com um numero:"));
+  if (inNumero == 0 || isNaN(inNumero)) {
+    alert("entre com um numero valido");
+  }
+} while (inNumero == 0 || isNaN(inNumero));
+let pares = `pares entre 1 e ${inNumero}`;
+for (let i = 2; i <= inNumero; i = i + 2) {
+  pares = pares + 1;
+}
+alert(pares);
---
+++ cap05/exerc-5.3/while.js
@@ -0,0 +1,8 @@
+//while => teste no inicio, devido o teste ser no inicio tem a possibilidade de não ser executado nenhuma vez
+
+let i = 0;
+
+while(i <= 10){
+  console.log(i);
+  i++;
+}
