060646e2a7c6a1959ee8f7fcf1b2c73c562294cd
Assis Souz4 <assissouza-dev@hotmail.com>
2023-08-13 13:15:58 -0300

    inplementando funcionalidade para mostrar alunos cadastrados

--- cap06/manipulacao-de-vetores-com-node/main.js
+++ cap06/manipulacao-de-vetores-com-node/main.js
@@ -4,13 +4,14 @@ const alunos = [];
 do {
   const nome = prompt("Digite seu Nome:");
   if (nome == "s") {
-    console.log("saiu");
+    // console.log("saiu");
     break;
   }
 
   const nota = Number(prompt("Digite sua Nota:"));
   alunos.push({ nome, nota });
-  console.log(alunos);
+  const lista = alunos.reduce((acc, aluno) => acc = aluno.nome, '');
+  console.log(`Aluno(a): ${lista}, cadastrado(a)...\n`);
 } while (true);
 console.log("-".repeat(50));
 
@@ -20,9 +21,9 @@ console.log(`Maior nota: ${maiorNota}`);
 if (maiorNota >= 7) {
   const alunosDestaques = alunos.filter((aluno) => aluno.nota >= 7 + "\n", "");
   console.log(`Alunos destaques com nota maior ou igual a 7`);
- for(const alunoDestaque of alunosDestaques) {
-  console.log(`Aluno : ${alunoDestaque.nome}`);
- }
-}else{
+  for (const alunoDestaque of alunosDestaques) {
+    console.log(`Aluno : ${alunoDestaque.nome}`);
+  }
+} else {
   console.log("Nenhum aluno tirou nota 7 ou maior");
 }
